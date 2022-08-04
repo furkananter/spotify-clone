ac2c79172206d107988a678acdbb21b34e430db9
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-08-04 19:42:05 +0300

    test

--- src/components/Sidebar/DownloadApp.js
+++ src/components/Sidebar/DownloadApp.js
@@ -4,7 +4,7 @@ function DownloadApp(){
     return (
         <div className="h-10 flex flex-shrink-0 text-sm font-semibold text-link gap-x-4 hover:text-white items-center px-6 ">
             <Icon name="download" size={20} />
-                Uygulamayı Yükle
+                Uygulamayı Yükleeee
         </div>
     );
 }
