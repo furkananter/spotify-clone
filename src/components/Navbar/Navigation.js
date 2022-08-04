d50749dc53ba6340013017d3b5feeb72abac9c00
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-08-04 23:42:47 +0300

    improvements 🎊

--- src/components/Navbar/Navigation.js
+++ src/components/Navbar/Navigation.js
@@ -1,12 +1,15 @@
 import { Icon } from 'Icons';
+import { useNavigate } from 'react-router-dom';
+
 
 function Navigation() {
+    const navigate = useNavigate();
     return (
-        <nav>
-            <button>
+        <nav className="flex items-center gap-x-4" >
+            <button onClick={() => {navigate(-1)}} className="w-8 h-8 justify-center flex items-center rounded-full bg-black bg-opacity-70">
                 <Icon name="previous" size={22} />
             </button>
-            <button>
+            <button onClick={() => {navigate(+1)}}  className="w-8 h-8 justify-center flex items-center rounded-full bg-black bg-opacity-70">
                 <Icon name="forward" size={22} />
             </button>
         </nav>
