157aced3b92b22912b612f8970af77f7163763a3
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-08-06 14:20:24 +0300

    Keep Coding 🧑‍💻

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,31 @@
 import { NavLink } from 'react-router-dom'
 
-function Section({ title, more = false }) {
+function Section({ title, more = false,items }) {
     return (
         <section>
             <header className="flex items-center justify-between">
                 <h3 className="text-2xl text-white font-semibold tracking-tight">{title}</h3>
                 {more && (
-                    <NavLink className="text-sm font-semibold text-link uppercase hover:underline" to={more}>
+                    <NavLink className="text-xs font-semibold text-link uppercase hover:underline" to={more}>
                         see all
                     </NavLink>
                 )
             }
             </header>
-            <div className="grid grid-cols-5">
-                <NavLink
-                 // key={"1"}
-                 to={"/"}>
-                </NavLink>
+            <div className="grid grid-cols-5 gap-x-6">
+                {items.map(item => (
+                    <NavLink
+                    className="bg-footer p-4 rounded"
+                    key={item.id}
+                    to={"/"}>
+
+                    <div className="pt-[100%] relative mb-4">
+                        <img className="absolute inset-0 w-full h-full rounded" src={item.image} alt={item.title} />
+                    </div>
+                    {item.title}
+                    </NavLink>
+                    
+                ))}
             </div>
         </section>
     )
--- src/views/Home.js
+++ src/views/Home.js
@@ -11,15 +11,27 @@ function Home(){
         },
         {
             id: 2,
-            title: 'Series & Movie Musics',
-            image: 'https://i.scdn.co/image/ab67706c0000da848cd36eb822c23457761dc3eb',
-            description: 'Ezel, Medcezir, Kuzey Güney, Yıldızlararası, Planet Earth vb...'
+            title: 'Road-trip 🚘',
+            image: 'https://i.scdn.co/image/ab67706c0000da84686abc7d15d1972b6765a6dc',
+            description: 'Road Road Road'
         },
         {
             id: 3,
-            title: 'Series & Movie Musics',
-            image: 'https://i.scdn.co/image/ab67706c0000da848cd36eb822c23457761dc3eb',
-            description: 'Ezel, Medcezir, Kuzey Güney, Yıldızlararası, Planet Earth vb...'
+            title: 'ee nabiyim',
+            image: 'https://i.scdn.co/image/ab67706c0000da844c621ab106de67d89b641cc9',
+            description: 'kaldı heveslerim kursağımda'
+        },
+        {
+            id: 4,
+            title: '<Code> 👨‍💻',
+            image: 'https://i.scdn.co/image/ab67706c0000da84bd86a6416b81fceeab89b5fe',
+            description: 'I usually listen this playlist when i code. There mostly no words in these songs.'
+        },
+        {
+            id: 5,
+            title: 'hypebeast 👟',
+            image: 'https://i.scdn.co/image/ab67706c0000da842b8a92a73ca3a70247282551',
+            description: 'Culture of Hypebeast'
         },
     ]
 
