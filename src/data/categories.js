8a834dfbe0cb0d7d9764c7ccc87547cda3ad10b9
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-08-12 12:34:53 +0300

    Uploading Categories 📄

---
+++ src/data/categories.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        id: '1',
+        title: 'Podcasts'
+    },
+    {
+        id: '2',
+        title: 'Podcasts'
+    },
+    {
+        id: '3',
+        title: 'Podcasts'
+    },
+
+]
