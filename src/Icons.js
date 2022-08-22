4c6fee5c27f163e3510b67b18f17604bfa432991
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-08-22 17:55:05 +0300

    Menu Active Icons Correction!

--- src/Icons.js
+++ src/Icons.js
@@ -89,6 +89,19 @@ const CollectionIcon = ({ size }) => {
     </svg>
   );
 }
+const ActiveCollectionIcon = ({ size }) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width={size}
+      height={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+      d="M3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zM15.5 2.134A1 1 0 0014 3v18a1 1 0 001 1h6a1 1 0 001-1V6.464a1 1 0 00-.5-.866l-6-3.464zM9 2a1 1 0 00-1 1v18a1 1 0 102 0V3a1 1 0 00-1-1z" fill="currentColor" />
+    </svg>
+  );
+}
 const PlusIcon = ({ size }) => {
   return (
     <svg
@@ -355,6 +368,7 @@ const Icon = ({ name, size = 24 }) => {
     search: SearchIcon,
     activeSearch: ActiveSearchIcon,
     collection: CollectionIcon,
+    collectionActive: ActiveCollectionIcon,
     plus: PlusIcon,
     heart: HeartIcon,
     heartFilled: HeartFilledIcon,
--- src/components/Sidebar/Menu.js
+++ src/components/Sidebar/Menu.js
@@ -1,7 +1,10 @@
 import { Icon } from 'Icons';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useMatch } from 'react-router-dom';
 
 function Menu() {
+    const homeButton = useMatch("/")
+    const searchButton = useMatch("/search")
+    const libraryButton = useMatch("/collection/playlists")
 
     return (
         <nav className="px-2">
@@ -11,7 +14,10 @@ function Menu() {
                         to={"/"}
                         className={({ isActive }) => "h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4" + (isActive ? ' active' : '')}>
                         <span>
-                            <Icon name="home" />
+                            {
+                                homeButton ? <Icon name="activeHome" /> : <Icon name="home" />
+                            }
+                            {/* <Icon name="home" /> */}
                         </span>
                         Home
                     </NavLink>
@@ -21,13 +27,15 @@ function Menu() {
                         to={"/search"}
                         className={({ isActive }) => "h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4" + (isActive ? ' active' : '')}>
                         <span>
-                            <Icon name="search" />
+                            {
+                                searchButton ? <Icon name="activeSearch" /> : <Icon name="search" />
+                            }
                         </span>
                         Search
                     </NavLink>
                 </li>
                 <li>
-                {/*
+                    {/*
                  //! If you don't even think about using "isActive", you should think!
                  Because you can't do this without "isActive". And if you don't use "isActive",
                  you can't give an active class to your NavLink.
@@ -42,12 +50,14 @@ function Menu() {
                      "h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4"
                      + (isActive ? ' active' : '')}
                 */}
-                    <NavLink 
+                    <NavLink
                         to={"/collection/playlists"}
-                        
+
                         className={({ isActive }) => "h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded hover:text-white px-4" + (isActive ? ' active' : ' ')}>
                         <span>
-                            <Icon name="collection" />
+                            {
+                                libraryButton ? <Icon name="collectionActive" /> : <Icon name="collection" />
+                            }
                         </span>
                         Your Library
                     </NavLink>
